eb0720bad9f0c209a79614a72d89a89a869da427
dionysus11 <dionysus11@naver.com>
2021-03-22 17:12:54 +0900

    Organize initial page Tabview(daily, weekly, monthly)

---
+++ lib/sub/weeklyFeedbackPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WeeklyFeedbackApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('세번째 페이지'),
+        ),
+      ),
+    );
+  }
+}
